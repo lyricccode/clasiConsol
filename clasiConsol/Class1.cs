739448af2fc850702c1bfc6c488f06ec788da38c
Demo <Demo@STUDENT-205-01>
2026-01-16 18:45:34 +0400

    Добавьте файлы проекта.

---
+++ clasiConsol/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasiConsol
+{
+    public class Book
+    {
+        string Author { get; set; }
+        string Name { get; set; }
+        int Year { get; set; }
+
+        
+
+        public Book(string author, string name, int year)
+        {
+            Author = author;
+            Name = name;
+            Year = year;
+        }
+
+        public void DisplayInfo()
+        {
+            int IsOld = 2026 - Year;
+            if (IsOld >= 20)
+            {
+                Console.WriteLine($" '{Name}' - {Author}, {Year} [Старая]");
+            }
+            else
+            {
+                Console.WriteLine($" '{Name}' - {Author}, {Year}");
+            }
+        }
+    }
+}
---
+++ clasiConsol/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace clasiConsol 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookObject = new Book("Михаил Булгаков", "Мастер и Маргарита", 1967);
+            bookObject.DisplayInfo();
+        }
+    }
+}
+
+
